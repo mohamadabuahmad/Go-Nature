c9ba7d43733010984fc619b6f1adcf403e7ec376
marwa <marwa@10.0.0.56>
2024-03-03 19:39:23 +0200

    GUI MARWA

---
+++ src/gui/ServiceRepresentativePage.java
@@ -0,0 +1,29 @@
+package gui;
+
+public class ServiceRepresentativePage {
+	
+
+		@FXML
+		private ImageView homePageButton;
+
+		@FXML
+		private ImageView addGuideButton;
+
+		@FXML
+		private ImageView addSubscriberButton;
+
+		@FXML
+		private Label pageTextLabel;
+
+		@FXML
+		private Label helloMessageLabel;
+
+		@FXML
+		private Button logOutButton;
+
+		@FXML
+		private AnchorPane anPane;
+
+		private ImageView selected;
+
+}
